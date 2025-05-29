64f5144029a36caff3001e8f9acdd81ec6a76ac5
LeeDongwon <ldw9981@gmail.com>
2025-05-29 15:28:09 +0900

    코드 추가

---
+++ UsingDLL/UsingDLL.cpp
@@ -0,0 +1,20 @@
+﻿// UsingDLL.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "../StaticImportDLL/StaticImportDLL.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "../x64/Debug/StaticImportDLL.lib")
+#else   
+#pragma comment(lib, "../x64/Release/StaticImportDLL.lib")
+#endif
+
+int main()
+{
+	int a = 10, b = 20;
+	int result = StaticImport_Add(a, b);
+
+
+    std::cout << "Hello World!\n";
+}
